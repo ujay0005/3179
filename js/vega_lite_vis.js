7431eb64bcd6558ba56bc741bad183dcddd314d4
Umethya <ujay0005@student.monash.edu>
2023-10-11 14:02:49 +1100

    push

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,3 +1,8 @@
 var vg_1 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/worldmap.vg.json";
-vegaEmbed("#vis", vg_1, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+vegaEmbed("#map", vg_1, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+
+var vg_2 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/worldmap.vg.json";
+vegaEmbed("#map", vg_1, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+
+
 
