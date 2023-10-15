7ab59ba6ee3c8b9095d0537b18b2cccd394f911c
Umethya <ujay0005@student.monash.edu>
2023-10-15 22:48:14 +1100

    p

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -4,12 +4,15 @@ vegaEmbed("#areachart", vg_1, {mode: "vega-lite"}).then(console.log).catch(conso
 var vg_2 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/piechart.vg.json";
 vegaEmbed("#piechart", vg_2, {mode: "vega-lite"}).then(console.log).catch(console.warn)
 
+var vg_3 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/bubblechart.vj.json";
+vegaEmbed("#bubblechart", vg_3, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+
+
 var vg_3 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/worldmap.vg.json";
 vegaEmbed("#worldmap", vg_3, {mode: "vega-lite"}).then(console.log).catch(console.warn)
 
 var vg_3 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/tempbar.vg.json";
 vegaEmbed("#tempbar", vg_3, {mode: "vega-lite"}).then(console.log).catch(console.warn)
-
 
 
 
