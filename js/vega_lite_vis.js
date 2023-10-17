6741ba337e352faa5212696e584dfa6e1f0be299
Umethya <ujay0005@student.monash.edu>
2023-10-17 19:35:53 +1100

    m

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,21 +1,21 @@
 var vg_1 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/ghg.vg.json";
-vegaEmbed("#areachart", vg_1, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+vegaEmbed("#areachart", vg_1, {mode: "vega-lite"},{"actions": false}).then(console.log).catch(console.warn)
 
 var vg_2 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/piechart.vg.json";
-vegaEmbed("#piechart", vg_2, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+vegaEmbed("#piechart", vg_2, {mode: "vega-lite"},{"actions": false}).then(console.log).catch(console.warn)
 
 var vg_3 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/bubblechart.vj.json";
-vegaEmbed("#bubblechart", vg_3, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+vegaEmbed("#bubblechart", vg_3, {mode: "vega-lite"},{"actions": false}).then(console.log).catch(console.warn)
 
 var vg_4 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/worldmap.vg.json";
-vegaEmbed("#worldmap", vg_4, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+vegaEmbed("#worldmap", vg_4, {mode: "vega-lite"},{"actions": false}).then(console.log).catch(console.warn)
 
 
 var vg_5 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/tempbar.vg.json";
-vegaEmbed("#tempbar", vg_5, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+vegaEmbed("#tempbar", vg_5, {mode: "vega-lite"},{"actions": false}).then(console.log).catch(console.warn)
 
 var vg_6 = "https://raw.githubusercontent.com/ujay0005/3179/main/json/stack.vg.json";
-vegaEmbed("#stack", vg_6, {mode: "vega-lite"}).then(console.log).catch(console.warn)
+vegaEmbed("#stack", vg_6, {mode: "vega-lite"},{"actions": false}).then(console.log).catch(console.warn)
 
 
 
